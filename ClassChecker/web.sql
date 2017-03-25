86519bd89a3cafe53364e9f573ce29e05b1a9950
bank <final.season7@windowslive.com>
2017-03-25 06:43:20 -0700

    no message

--- ClassChecker/web.sql
+++ ClassChecker/web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 25, 2017 at 02:18 PM
+-- Generation Time: Mar 25, 2017 at 02:42 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.0.15
 
@@ -23,6 +23,106 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `text` varchar(100) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `sec_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `grade`
+--
+
+CREATE TABLE `grade` (
+  `grade` float NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `sub_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `qrcode`
+--
+
+CREATE TABLE `qrcode` (
+  `qrcode_id` int(15) NOT NULL,
+  `sec_id` int(11) NOT NULL,
+  `mfd` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `exp` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `section`
+--
+
+CREATE TABLE `section` (
+  `sec_id` int(11) NOT NULL,
+  `sub_id` int(11) NOT NULL,
+  `year` int(11) NOT NULL,
+  `semester` int(11) NOT NULL,
+  `description` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `sub_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `credit` int(11) NOT NULL,
+  `time` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `takesclass`
+--
+
+CREATE TABLE `takesclass` (
+  `date` datetime NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `qrcode_id` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `takescourse`
+--
+
+CREATE TABLE `takescourse` (
+  `username` varchar(10) NOT NULL,
+  `sec_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachcourse`
+--
+
+CREATE TABLE `teachcourse` (
+  `role` varchar(10) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `sec_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -36,8 +136,63 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`username`, `password`, `fname`, `lname`, `role`, `path_pic`) VALUES
+('5710404322', '1234', 'chaiwat', 'prapanwong', 'admin', '');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`username`,`sec_id`);
+
+--
+-- Indexes for table `grade`
+--
+ALTER TABLE `grade`
+  ADD PRIMARY KEY (`username`,`sub_id`);
+
+--
+-- Indexes for table `qrcode`
+--
+ALTER TABLE `qrcode`
+  ADD PRIMARY KEY (`qrcode_id`);
+
+--
+-- Indexes for table `section`
+--
+ALTER TABLE `section`
+  ADD PRIMARY KEY (`sec_id`,`sub_id`);
+
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`sub_id`);
+
+--
+-- Indexes for table `takesclass`
+--
+ALTER TABLE `takesclass`
+  ADD PRIMARY KEY (`username`,`qrcode_id`);
+
+--
+-- Indexes for table `takescourse`
+--
+ALTER TABLE `takescourse`
+  ADD PRIMARY KEY (`username`,`sec_id`);
+
+--
+-- Indexes for table `teachcourse`
+--
+ALTER TABLE `teachcourse`
+  ADD PRIMARY KEY (`username`,`sec_id`);
 
 --
 -- Indexes for table `user`
