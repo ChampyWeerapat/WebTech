2360e4f3a3c23d58ea97cb126ee0f572c3235c0b
bank <final.season7@windowslive.com>
2017-03-26 03:23:51 -0700

    no message

--- ClassChecker/admin.php
+++ ClassChecker/admin.php
@@ -6,19 +6,19 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Loading Bootstrap -->
-    <link href="Flat-UI-master/dist/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="file:///C|/xampp/htdocs/ClassChecker/Flat-UI-master/dist/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
 
     <!-- Loading Flat UI -->
-    <link href="Flat-UI-master/dist/css/flat-ui.css" rel="stylesheet">
-    <link href="Flat-UI-master/docs/assets/css/demo.css" rel="stylesheet">
-    <link rel="shortcut icon" href="Flat-UI-master/img/favicon.ico">
-    <link rel="stylesheet" type="text/css" href="CSS/admin_style.css">
+    <link href="file:///C|/xampp/htdocs/ClassChecker/Flat-UI-master/dist/css/flat-ui.css" rel="stylesheet">
+    <link href="file:///C|/xampp/htdocs/ClassChecker/Flat-UI-master/docs/assets/css/demo.css" rel="stylesheet">
+    <link rel="shortcut icon" href="file:///C|/xampp/htdocs/ClassChecker/Flat-UI-master/img/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="file:///C|/xampp/htdocs/ClassChecker/CSS/admin_style.css">
 
 
-    <script src="Flat-UI-master/dist/js/vendor/jquery.min.js"></script>
-    <script src="Flat-UI-master/dist/js/flat-ui.min.js"></script>
-    <script src="Flat-UI-master/docs/assets/js/application.js"></script>
+    <script src="file:///C|/xampp/htdocs/ClassChecker/Flat-UI-master/dist/js/vendor/jquery.min.js"></script>
+    <script src="file:///C|/xampp/htdocs/ClassChecker/Flat-UI-master/dist/js/flat-ui.min.js"></script>
+    <script src="file:///C|/xampp/htdocs/ClassChecker/Flat-UI-master/docs/assets/js/application.js"></script>
     
     <script>
       $(document).ready(function () {
@@ -62,7 +62,7 @@
               <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
                 <span class="sr-only">Toggle navigation</span>
               </button>
-              <a class="navbar-brand" href="home.php">ClassChecker</a>
+              <a class="navbar-brand" href="file:///C|/xampp/htdocs/ClassChecker/home.php">ClassChecker</a>
             </div>
             <div class="collapse navbar-collapse" id="navbar-collapse-01">
               <ul class="nav navbar-nav navbar-left">
@@ -70,21 +70,21 @@
                </ul>
                <ul class="nav navbar-nav navbar-center">
                 <li><a href="#fakelink">About</a></li>
-                <li><a href="contact.php">Contact Us</a></li>
+                <li><a href="file:///C|/xampp/htdocs/ClassChecker/contact.php">Contact Us</a></li>
                </ul>
 
                 <ul class="nav navbar-nav navbar-right">
                   <li><a href="#"><span class="fui-mail"></span><span class="navbar-unread"></span></a></li>
-                	<li><a href="profile.php"><img class="profile-icon" src="https://www.bodynbrain.com/img/unknownProfile.png" /><span class="user">TestUser</span></a></li>
+                	<li><a href="file:///C|/xampp/htdocs/ClassChecker/profile.php"><img class="profile-icon" src="https://www.bodynbrain.com/img/unknownProfile.png" /><span class="user">TestUser</span></a></li>
           			<li class="dropdown">
                   	<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="fui-gear"></span></a>
                   	<span class="dropdown-arrow"></span>
                   	<ul class="dropdown-menu">
-                    	<li><a href="profile.php"><span class="fui-user"></span>  Profile</a></li>
-                    	<li><a href="setting.php"><span class="fui-lock"></span>  Settings</a></li>
+                    	<li><a href="file:///C|/xampp/htdocs/ClassChecker/profile.php"><span class="fui-user"></span>  Profile</a></li>
+                    	<li><a href="file:///C|/xampp/htdocs/ClassChecker/setting.php"><span class="fui-lock"></span>  Settings</a></li>
 	                    <li><a href="#"><span class="fui-question-circle"></span>  Help</a></li>
 	                    <li class="divider"></li>
-	                    <li><a href="login.php"><span class="fui-power"></span>  Log out</a></li>
+	                    <li><a href="file:///C|/xampp/htdocs/ClassChecker/login.php"><span class="fui-power"></span>  Log out</a></li>
                   </ul>
                 </li>
                </ul>
@@ -141,6 +141,10 @@
               <option value="Summer">Summer</option>
             </select>
         </label>
+        <form action="file:///C|/xampp/htdocs/ClassChecker/importData.php" method="post" enctype="multipart/form-data" id="importFrm">
+                <input type="file" name="file" />
+           
+            </form>
         <hr>
         <button  class="btn btn-block btn-lg btn-info" id="btn-save">Submit</button>
         </form>
