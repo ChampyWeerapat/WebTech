466b3a476ab5fbdcd1aea2017d9c89dbe40ca2e1
dizzenter <sirasith.k@ku.th>
2017-03-27 10:35:38 +0700

    Update admin : btn

--- ClassChecker/admin.php
+++ ClassChecker/admin.php
@@ -8,31 +8,29 @@
     <!-- Loading Bootstrap -->
     <link href="Flat-UI-master/dist/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
+
     <!-- Loading Flat UI -->
     <link href="Flat-UI-master/dist/css/flat-ui.css" rel="stylesheet">
     <link href="Flat-UI-master/docs/assets/css/demo.css" rel="stylesheet">
-    <link rel="shortcut icon" href="Flat-UI-master/img/favicon.ico">
+    <link rel="shortcut icon" href="file:///C|/xampp/htdocs/ClassChecker/Flat-UI-master/img/favicon.ico">
     <link rel="stylesheet" type="text/css" href="CSS/admin_style.css">
 
-    <!-- JavaScript -->
+
     <script src="Flat-UI-master/dist/js/vendor/jquery.min.js"></script>
     <script src="Flat-UI-master/dist/js/flat-ui.min.js"></script>
     <script src="Flat-UI-master/docs/assets/js/application.js"></script>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script type='text/javascript' src='JS/scriptHome.js'></script>
     
     <script>
       $(document).ready(function () {
      $("#upload-csv").hover(
         function() // on mouseover
         {
-            $(this).text(".CSV");
+            $('#text-btn-import').text(".CSV");
         }, 
 
         function() // on mouseout
         {
-            $(this).text("IMPORT");
+            $('#text-btn-import').text("IMPORT");
 
         });
       });
@@ -54,6 +52,8 @@
         e.preventDefault();
     });
 });
+
+
     </script>
 </head>
 <body>
@@ -64,7 +64,7 @@
               <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-01">
                 <span class="sr-only">Toggle navigation</span>
               </button>
-              <a class="navbar-brand" id="home-navbar">ClassChecker</a>
+              <a class="navbar-brand" href="admin.php">ClassChecker</a>
             </div>
             <div class="collapse navbar-collapse" id="navbar-collapse-01">
               <ul class="nav navbar-nav navbar-left">
@@ -75,22 +75,36 @@
                 <li><a href="contact.php">Contact Us</a></li>
                </ul>
 
-                <?php 
-                  include 'PHP/info_navbar.php';
-                ?>
-                
+                <ul class="nav navbar-nav navbar-right">
+                  <li><a href="#"><span class="fui-mail"></span><span class="navbar-unread"></span></a></li>
+                	<li><a href="profile.php"><img class="profile-icon" src="https://www.bodynbrain.com/img/unknownProfile.png" /><span class="user">TestUser</span></a></li>
+          			<li class="dropdown">
+                  	<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="fui-gear"></span></a>
+                  	<span class="dropdown-arrow"></span>
+                  	<ul class="dropdown-menu">
+                    	<li><a href="profile.php"><span class="fui-user"></span>  Profile</a></li>
+                    	<li><a href="setting.php"><span class="fui-lock"></span>  Settings</a></li>
+	                    <li><a href="#"><span class="fui-question-circle"></span>  Help</a></li>
+	                    <li class="divider"></li>
+	                    <li><a href="index.php"><span class="fui-power"></span>  Log out</a></li>
+                  </ul>
+                </li>
+               </ul>
             </div><!-- /.navbar-collapse -->
           </nav><!-- /navbar -->
           </div>
   </div>
 	<div class="wrapper">
-   <button class="btn btn-block btn-lg btn-warning" id ="create-user">CREATE USER</button>
-   <button class="btn btn-block btn-lg btn-info" id ="manage-user">MANAGE USER</button>
-  <button class="btn btn-block btn-lg btn-primary" data-popup-open="popup-1"  id ="upload-csv">IMPORT</button>
+   <button class="btn btn-block btn-lg btn-warning" data-popup-open="popup-create" id ="create-user"><span class="fui-plus"> </span>CREATE USER</button>
+
+   <button class="btn btn-block btn-lg btn-info" data-popup-open="popup-create-from-csv" id ="create-user-csv"><span class="fui-exit"> </span>CREATE USER (CSV)</button>
+
+  <button class="btn btn-block btn-lg btn-primary" data-popup-open="popup-import"  id ="upload-csv"><span class="fui-folder"> </span><label id="text-btn-import">IMPORT</label></button>
+
   </div>
   <div class="box" style="width: 900px; height: 500px; overflow: auto">
-  <div class="table-user">
-    <table border="1">
+  <div class="wrapper-table">
+    <table border="1" id="table-info">
       <col width="150px" />
       <col width="250px" />
       <col width="250px" />
@@ -112,31 +126,59 @@
             <option value="Teacher">Teacher</option>  
           </select>
          </td>
-        <td><button class="fui-document"></button></td>
+        <td><button class="fui-document" id="export-each"></button></td>
       </tr>
     </table>
   </div>
+    <button class="btn btn-block btn-lg btn-info" id="export-all"><span class="fui-document"></span> All User</button>
   </div>
 
 
-  <div class="popup" data-popup="popup-1">
+  <div class="popup" data-popup="popup-import">
     <div class="popup-inner">
-      <form action="PHP/importData.php" method="post" enctype="multipart/form-data" id="importFrm">
-        <label><h6>Enter Subject ID</h6><input type="text" name="subId" value="" placeholder="Subject ID Ex. 01418433" class="form-control" /></label>
-        <label><h6>Year</h6><input type="text" name="year" value="" placeholder="Year Ex.2560" class="form-control" /></label>
+      <form>
+        <label><h6>Enter Subject ID</h6><input type="text" value="" placeholder="Subject ID Ex. 01418433" class="form-control" /></label>
+        <label><h6>Year</h6><input type="text" value="" placeholder="Year Ex.2560" class="form-control" />
+        </label>
         <label><h6>Semester</h6>
         <select name="semester">
-              <option  value="First">First </option>
-              <option  value="Second">Second</option>
-              <option  value="Summer">Summer</option>
+              <option value="First">First </option>
+              <option value="Second">Second</option>
+              <option value="Summer">Summer</option>
             </select>
         </label>
-        <input type="file" name="file" /> 
+        <form action="PHP/importData.php" method="post" enctype="multipart/form-data" id="importFrm">
+                <input type="file" name="file" value="" class="form-control" />  
+            </form>
         <hr>
-        <input type="submit" class="btn btn-block btn-lg btn-info" name="importSubmit" value="IMPORT">
+        <button  class="btn btn-block btn-lg btn-info" type="Submit" id="btn-save">Submit</button>
         </form>
-        <a class="popup-close" data-popup-close="popup-1" href="#">x</a>
+        <a class="popup-close" data-popup-close="popup-import" href="#">x</a>
     </div>
 </div>
+
+<!-- Create User -->
+<div class="popup" data-popup="popup-create">
+    <div class="popup-inner">
+      <form>
+          <label><h6>New User ID</h6><input type="text" value="" placeholder="New User ID" class="form-control" id="new-user" /></label>
+        <hr>
+        <button  class="btn btn-block btn-lg btn-info" id="btn-save">Create New User</button>
+        </form>
+        <a class="popup-close" data-popup-close="popup-create" href="#">x</a>
+    </div>
+  </div>
+
+<!-- Create User from csv -->
+    <div class="popup" data-popup="popup-create-from-csv">
+    <div class="popup-inner">
+      <form>
+          <label><h6>New User ID File (.CSV)</h6><input type="file" name="file" value="" class="form-control" /></label>
+        <hr>
+        <button  class="btn btn-block btn-lg btn-info" id="btn-save">Create New User</button>
+        </form>
+        <a class="popup-close" data-popup-close="popup-create-from-csv" href="#">x</a>
+    </div>
+    </div>
 </body>
 </html>
