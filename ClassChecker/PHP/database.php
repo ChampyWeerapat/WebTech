6f94ac36bd72dd23df98865be825437fa4763994
bank <final.season7@windowslive.com>
2017-03-27 08:03:50 -0700

    update database

--- ClassChecker/PHP/database.php
+++ ClassChecker/PHP/database.php
@@ -38,6 +38,22 @@ public function getPassword($username, $password) {
     }
     return $stmt->fetch();
   }
+   public function getDateQr($qrcode_id,$sec_id,$sub_id){
+	  
+    try {
+      $sql = "SELECT date FROM user WHERE qrcode_id = '$qrcode_id' and sec_id = '$sec_id' and  sub_id = '$sub_id'";
+      $stmt = $this->connection->query($sql);
+      $count = $stmt->rowCount();
+      $date = '';
+      if ($count == 1) {
+        $row = $stmt->fetch();
+        $date = $row["date"];
+      }
+    }  catch (Exception $e) {
+      die($e->getMessage());
+    }
+    return $date;
+   }
   public function getAccount() {
     try {
       $sql = 'SELECT * FROM user';
@@ -156,7 +172,7 @@ public function getPassword($username, $password) {
   
   public function insertTakeClass($date,$status,$username,$qrcode_id){
 	  try {
-	 $sql = "INSERT INTO takeclass (date,status,username,qrcode_id)
+	 $sql = "INSERT INTO takesclass (date,status,username,qrcode_id)
     VALUES ('$date','$status','$username','$qrcode_id')";
 	$stmt = $this->connection->query($sql);
 	//echo "New record created successfully";
